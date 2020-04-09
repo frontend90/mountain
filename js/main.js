8ab3ab959c9c5f32282d44d775ff3d5fb9cf7fce
frontend90 <leon44173@gmail.com>
2020-04-09 07:13:35 +0300

    верстка

--- js/main.js
+++ js/main.js
@@ -8,3 +8,63 @@ $('.close').click(function () {
   $('.menu-overlay').removeClass('show').css("transition-delay", ".5s");
   $('.menu-link').removeClass('show');
 });
+
+$('.menu-link a').click(function () {
+  $('.menu-overlay').removeClass('show').css("transition-delay", ".5s");
+  $('.menu-link').removeClass('show');
+});
+
+/*************    odometer    *******************/
+setTimeout(function () {
+  $('.odometer.01').html(23578);
+}, 1000);
+setTimeout(function () {
+  $('.odometer.02').html(12702);
+}, 1000);
+
+/*********************  плавный скролл    ***************/
+$(document).ready(function () {
+  $('a[href^="#"]').click(function () {
+    elementClick = $(this).attr("href");
+    destination = $(elementClick).offset().top;
+    if ($.browser) {
+      $('body').animate({
+        scrollTop: destination
+      }, 1000);
+    } else {
+      $('html').animate({
+        scrollTop: destination
+      }, 1000);
+    }
+    return false;
+  });
+});
+
+/*************    portfolio modal      ************/
+$('[data-modal]').click(function () {
+  event.preventDefault();
+
+  let $this = $(this);
+  let modalId = $this.data('modal');
+
+  $(modalId).addClass('show');
+  $('body').addClass('no-scroll');
+});
+
+$('.close').click(function () {
+  event.preventDefault();
+
+  let $this = $(this);
+  let modalParent = $this.parents('.portfolio-modal');
+
+  modalParent.removeClass('show');
+  $('body').removeClass('no-scroll');
+});
+
+$(document).on('click', function (e) {
+  if (!$(e.target).closest(".block img").length) {
+    $('.portfolio-modal').removeClass('show');
+    $('body').removeClass('no-scroll');
+  }
+  e.stopPropagation();
+});
